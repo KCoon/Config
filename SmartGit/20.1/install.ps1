76eb361406aae644761f1086633d4a535cfc01bc
Sven Langer <mail@svenlanger.org>
2020-09-04 22:29:17 +0200

    Backup script

---
+++ SmartGit/20.1/install.ps1
@@ -0,0 +1,8 @@
+Set-Location $PSScriptRoot
+
+$smartgitDir = Join-Path $env:APPDATA syntevo\SmartGit\20.1
+$backupPath = Join-Path $smartgitDir ".configBackup"
+
+New-Item -Path $backupPath -ItemType Directory -Force
+Copy-Item -Path (Join-Path $smartgitDir .\tools.yml) -Destination $backupPath
+Copy-Item -Path (Join-Path $smartgitDir .\ui-config.yml) -Destination $backupPath
