61d7d3f9b191780567b5cbaec27b5cf469b7a943
Sven Langer <mail@svenlanger.org>
2020-09-05 07:56:54 +0200

    install

--- SmartGit/20.1/install.ps1
+++ SmartGit/20.1/install.ps1
@@ -6,3 +6,6 @@ $backupPath = Join-Path $smartgitDir ".configBackup"
 New-Item -Path $backupPath -ItemType Directory -Force
 Copy-Item -Path (Join-Path $smartgitDir .\tools.yml) -Destination $backupPath
 Copy-Item -Path (Join-Path $smartgitDir .\ui-config.yml) -Destination $backupPath
+
+Copy-Item -Path "tools.yml" -Destination $smartgitDir
+Copy-Item -Path "ui-config.yml" -Destination $smartgitDir
